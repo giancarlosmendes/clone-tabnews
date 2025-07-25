29927acb1478ad19adeb1ba3a4f21b2efc226252
Giancarlos Mendes <92035791+giancarlosmendes@users.noreply.github.com>
2025-07-25 06:21:49 +0000

    atualiza a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Pincesa linda, eu amo você. Se você me ama, dá uma risadinha! 🤭</h1>
+    return <h1>Pincesa linda, eu amo você. Se você me ama, dá uma risadinha! 🤭❤️</h1>
 }
 
 export default Home;
