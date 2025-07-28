eb4d3d6a0d9664499797d847486080583e41ab3b
Giancarlos Mendes <92035791+giancarlosmendes@users.noreply.github.com>
2025-07-28 19:23:30 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Pincesa linda, eu amo você. Se você me ama, dá uma risadinha! 🤭❤️</h1>
+  return (
+    <h1>Pincesa linda, eu amo você. Se você me ama, dá uma risadinha! 🤭❤️</h1>
+  );
 }
 
 export default Home;
